7de09c7830998c70e8845743b701b926dcf86b88
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2021-12-13 11:43:18 +0100

    New run settings

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ original_parameter_values: Dict[str, float] = {
 }
 min_flux = 0.01
 max_scaling = 1000
-num_batches = 10
+num_batches = 5
 num_runs_per_batch = 10_000
 results: List[Dict[str, float]] = []
 for _ in range(num_batches):
@@ -168,7 +168,7 @@ for _ in range(num_batches):
     new_results = ray.get(futures)
     results += new_results
 
-random.seed(34567890)
+random.seed(567890)
 
 results_list_dict: Dict[str, List[float]] = {}
 for key in selections + ["extra_data"]:
