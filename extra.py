f3441146289ba31121fa0cc4dd9775e85b962c0e
Paulocracy <36934614+Paulocracy@users.noreply.github.com>
2021-12-17 11:32:26 +0100

    Reordering and extended statistics

--- extra.py
+++ extra.py
@@ -1,5 +1,6 @@
+import statistics
 from helper import json_zip_load, save_boxplot
-dictionary = json_zip_load("./statistics_20000/TEST.json")
+dictionary = json_zip_load("./statistics_50000/TEST.json")
 
 rel_flux_adv = dictionary["relative_community_flux_advantage"]
 abs_flux_adv = dictionary["absolute_community_flux_advantage"]
@@ -7,27 +8,20 @@ rel_mmdf_adv = dictionary["relative_community_mmdf_advantage"]
 abs_mmdf_adv = dictionary["absolute_community_mmdf_advantage"]
 xratio = dictionary["community_B_to_community_A_metabolite_X_ratio"]
 
-advantages_sum_xratio_gt_1 = 0.0
-advantages_num_xratio_gt_1 = 0
-advantages_sum_xratio_lt_1 = 0.0
-advantages_num_xratio_lt_1 = 0
 advantages_gt_1 = []
 advantages_lt_1 = []
 for index in range(len(rel_flux_adv)):
-    if rel_flux_adv[index] <= 1:
+    if rel_flux_adv[index] <= 1.25:
         continue
 
     if xratio[index] > 1.0:
-        advantages_sum_xratio_gt_1 += rel_flux_adv[index]
-        advantages_num_xratio_gt_1 += 1
         advantages_gt_1.append(rel_flux_adv[index])
     else:
-        advantages_sum_xratio_lt_1 += rel_flux_adv[index]
-        advantages_num_xratio_lt_1 += 1
         advantages_lt_1.append(rel_flux_adv[index])
 
-save_boxplot("./TEST.png", advantages_lt_1, "", "", "")
-print(advantages_num_xratio_gt_1)
-print(advantages_sum_xratio_gt_1 / advantages_num_xratio_gt_1)
-print(advantages_num_xratio_lt_1)
-print(advantages_sum_xratio_lt_1 / advantages_num_xratio_lt_1)
+print(len(advantages_gt_1))
+print(sum(advantages_gt_1) / len(advantages_gt_1))
+print(statistics.median(advantages_gt_1))
+print(len(advantages_lt_1))
+print(sum(advantages_lt_1) / len(advantages_lt_1))
+print(statistics.median(advantages_lt_1))
